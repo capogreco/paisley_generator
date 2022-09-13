58781f7518cde0f0af153805d133cd20f9c7f8db
Stew <stewartcollins7@protonmail.com>
2022-09-13 21:41:18 +1000

    fix: refactor paisley tail generator into function

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,37 @@ fn model(_app: &App) -> Model {
 fn update(_app: &App, _model: &mut Model, _update: Update) {
 }
 
+fn get_wiggler_tail(width: i32, wiggle_rate: f32, wiggle_intensity: f32, tail_length: usize, time: f32) -> Vec<Point2> {
+    let twirlAngle = time * TAU * wiggle_rate;
+    let twirl = twirlAngle.sin() * PI / 2.0;
+
+    let mut side1 : Vec<Point2> = Vec::with_capacity(tail_length);
+    let mut side2 : Vec<Point2> = Vec::with_capacity(tail_length);
+
+    for i in 1..tail_length {
+        let position = i as f32 / tail_length as f32;
+        let position_width = width as f32 * (position * PI / 2.0).cos().pow(4);
+        let theta = position * &wiggle_intensity * twirl;
+        let angle = (PI / 2.0) + theta;
+        let x1 = angle.cos() * i as f32;
+        let y1 = angle.sin() * i as f32;
+        let p1 = pt2(x1, y1);
+
+        let x2 = (angle + PI/2.0).cos() * position_width / 2.0 as f32;
+        let y2 = (angle + PI/2.0).sin()* position_width / 2.0 as f32;
+        let p2 = pt2(x2, y2) + p1;
+        side1.push(p2);
+
+        let x3 = (angle - PI/2.0).cos() * position_width / 2.0 as f32;
+        let y3 = (angle - PI/2.0).sin() * position_width / 2.0 as f32;
+        let p3 = pt2(x3, y3) + p1;
+        side2.push(p3);
+    }
+
+    side1.append(&mut side2);
+    side1
+}
+
 fn view(_app: &App, _model: &Model, frame: Frame){
     frame.clear(PINK);
 
@@ -25,66 +56,11 @@ fn view(_app: &App, _model: &Model, frame: Frame){
 
     draw.ellipse().width(100.0).height(100.0);
 
-    let time = _app.time;
+    let points = get_wiggler_tail(100, 1.5, 0.5, 300, _app.time);
 
-    // Width?
-    let wid = 100;
-    // Rate of Wiggle
-    let rat: f64 = 1.5;
-    // Turn
-    let trn: f64 =  0.5;
-    let twirlAngle = time as f64 * TAU as f64* rat as f64;
-    let twirl = twirlAngle.sin() as f32 * PI / 2.0;
-
-    const tailLength: usize = 300;
-    let mut points1: [Vec2; tailLength] = [pt2(0.0,0.0); tailLength];
-    let mut points2: [Vec2; tailLength] = [pt2(0.0,0.0); tailLength];
-    let mut points3: [Vec2; tailLength] = [pt2(0.0,0.0); tailLength];
-    for i in 1..tailLength {
-        let p = i as f64 / tailLength as f64;
-        let w = wid as f64 * (p * PI as f64 / 2.0 as f64).cos().pow(4);
-        let theta = p * trn as f64 * twirl as f64;
-        let angle = (PI / 2.0) as f64 + theta;
-        if i < 20 {
-            println!("i: {}, w: {}, theta: {}, angle: {}", i, w, theta, angle)
-        }
-        let x1 = angle.cos() as f32 * i as f32;
-        let y1 = angle.sin() as f32 * i as f32;
-        // let p1: Vec2 = Vec2::from([x, y]);
-        let p1 = pt2(x1, y1);
-        points1[i] = p1;
-        let x2 = (angle as f32 + PI/2.0).cos() as f32 * w as f32 / 2.0 as f32;
-        let y2 = (angle as f32 + PI/2.0).sin() as f32 * w as f32 / 2.0 as f32;
-        // let p1: Vec2 = Vec2::from([x, y]);
-        let p2 = pt2(x2, y2) + p1;
-        points2[i] = p2;
-
-        let x3 = (angle as f32 - PI/2.0).cos() as f32 * w as f32 / 2.0 as f32;
-        let y3 = (angle as f32 - PI/2.0).sin() as f32 * w as f32 / 2.0 as f32;
-        // let p1: Vec2 = Vec2::from([x, y]);
-        let p3 = pt2(x3, y3) + p1;
-        points3[i] = p3;
-        // points.push((x as f32, y as f32));
-        // print!("{:?}", p1);
-        // print!("hi");
-        // p1;
-    }
-    // Create a polyline builder. Hot-tip: polyline is short-hand for a path that is
-    // drawn via "stroke" tessellation rather than "fill" tessellation.
-    // draw.polyline()
-    //     .weight(3.0)
-    //     .points(points1);
     draw.polyline()
         .weight(3.0)
-        .points(points2);
-    draw.polyline()
-        .weight(3.0)
-        .points(points3);
-    // if time < 5.0 {
-    //     println!("points {:?}", points);
-    // }
-
-    // println!("bye");
+        .points(points);
 
     draw.to_frame(_app, &frame).unwrap();
 }
