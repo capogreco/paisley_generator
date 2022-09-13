0be97f656a597b3f5f7e0a013549b73be17e46f4
Stew <stewartcollins7@protonmail.com>
2022-09-13 22:52:29 +1000

    feat: add double layer wiggler

--- src/main.rs
+++ src/main.rs
@@ -46,8 +46,12 @@ fn get_wiggler_tail(width: i32, wiggle_rate: f32, wiggle_intensity: f32, tail_le
         side2.push(p3);
     }
 
-    side1.append(&mut side2);
-    side1
+    let mut reversed_side_1 = Vec::with_capacity(tail_length);
+    for i in 0..side1.len() {
+        reversed_side_1.push(side1[side1.len() - 1 - i])
+    }
+    side2.append(&mut reversed_side_1);
+    side2
 }
 
 fn view(_app: &App, _model: &Model, frame: Frame){
@@ -55,18 +59,36 @@ fn view(_app: &App, _model: &Model, frame: Frame){
 
     let draw = _app.draw();
 
-    let circle_start = pt2(-50.0, 0.0);
-    let circle_end = pt2(50.0, 0.0);
+    let circle_start = pt2(-75.0, 0.0);
+    let circle_end = pt2(75.0, 0.0);
     let mut semi_circle_points = geometry_functions::semi_circle(circle_end, circle_start);
 
     // draw.ellipse().width(100.0).height(100.0);
 
-    let mut points = get_wiggler_tail(100, 1.5, 0.5, 300, _app.time);
+    let mut wiggler_tail = get_wiggler_tail(150, 1.1, 0.5, 300, _app.time);
+    let mut points = Vec::with_capacity(300 * 2 + 180 + 1);
+    let meeting_point = pt2(wiggler_tail[0].x, wiggler_tail[0].y);
+    points.append(&mut wiggler_tail);
     points.append(&mut semi_circle_points);
+    points.push(meeting_point);
 
-    draw.polyline()
-        .weight(3.0)
-        .points(points);
+    let circle_start_2 = pt2(-37.5, 0.0);
+    let circle_end_2 = pt2(37.5, 0.0);
+    let mut semi_circle_points_2 = geometry_functions::semi_circle(circle_end_2, circle_start_2);
+
+    let mut wiggler_tail_2 = get_wiggler_tail(75, 1.1, 0.5, 300, _app.time);
+    let mut points_2 = Vec::with_capacity(150 * 2 + 180 + 1);
+    let meeting_point_2 = pt2(wiggler_tail_2[0].x, wiggler_tail_2[0].y);
+    points_2.append(&mut wiggler_tail_2);
+    points_2.append(&mut semi_circle_points_2);
+    points_2.push(meeting_point_2);
+
+    draw.polygon()
+        .points(points)
+        .color(PURPLE);
+    draw.polygon()
+        .points(points_2)
+        .color(LIMEGREEN);
 
     draw.to_frame(_app, &frame).unwrap();
 }
