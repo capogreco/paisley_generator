214cccde8fcdfa321f4b1e905e58486dbe7fd6f3
Stew <stewartcollins7@protonmail.com>
2022-09-13 21:46:18 +1000

    fix: update function types

--- src/lib/geometry_functions.rs
+++ src/lib/geometry_functions.rs
@@ -2,12 +2,12 @@ extern crate nannou;
 use nannou::prelude::*;
 use nannou::prelude::geom::Vec2;
 
-fn semi_circle(start_point :: Vec2, end_point: Vec2) -> [Vec2: 180] {
+fn semi_circle(start_point: Vec2, end_point: Vec2) -> [Vec2; 180] {
     let diameter = end_point - start_point;
     let radius = diameter * 0.5;
     let mid_point = start_point + radius;
 
-    let mut points = [Vec2: 180] = [pt2(0.0, 0.0); 180];
+    let mut points : [Vec2; 180] = [pt2(0.0, 0.0); 180];
 
     for i in 0..180 {
         let angle = i * PI / 180;
@@ -17,5 +17,5 @@ fn semi_circle(start_point :: Vec2, end_point: Vec2) -> [Vec2: 180] {
         points[i] = p;
     }
 
-    return points;
+    points
 }
