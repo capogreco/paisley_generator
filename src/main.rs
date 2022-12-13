993fe69298d0e8e90084f39d4f249f149388991c
Stew <stewartcollins7@protonmail.com>
2022-12-13 21:11:17 +1100

    fix: return points from wiggler function

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,16 @@ fn model(_app: &App) -> Model {
 fn update(_app: &App, _model: &mut Model, _update: Update) {
 }
 
+/*
+    @param width the width of the wiggler at the widest point
+    @param wiggle_rate the speed that the wiggler completes one full arc in seconds
+    @param wiggle_intensity the maximum angle that the tail sweeps around to
+    @param time the time variable of the frame in which it is drawmn
+    @param tail_length the length of the tail from the centre of the semi circle head
+    @param x_offset the amount to offset all points by in the x axis
+    @param y_offset the amount to offset all points by in the y axis
+    @returns an array of points of a completed wiggler tail
+ */
 fn get_wiggler_tail(width: i32, wiggle_rate: f32, wiggle_intensity: f32, tail_length: usize, time: &f32, x_offset: &f32, y_offset: &f32) -> Vec<Point2> {
     let twirl_angle = time * TAU * wiggle_rate;
     let twirl = twirl_angle.sin() * PI / 2.0;
@@ -53,7 +63,7 @@ fn get_wiggler_tail(width: i32, wiggle_rate: f32, wiggle_intensity: f32, tail_le
     side2
 }
 
-fn draw_wiggler(draw: &Draw, width: i32, wiggle_rate: f32, wiggle_intensity: f32, tail_length: usize, time: &f32, x_offset: &f32, y_offset: &f32){
+fn get_wiggler_points(draw: &Draw, width: i32, wiggle_rate: f32, wiggle_intensity: f32, tail_length: usize, time: &f32, x_offset: &f32, y_offset: &f32) -> Vec<Point2>{
     let circle_start = pt2(width.clone() as f32 / -2.0 + x_offset, 0.0 + y_offset);
     let circle_end = pt2(width.clone() as f32 / 2.0 + x_offset, 0.0 + y_offset);
     let mut semi_circle_points = geometry_functions::semi_circle(circle_end, circle_start);
@@ -67,25 +77,7 @@ fn draw_wiggler(draw: &Draw, width: i32, wiggle_rate: f32, wiggle_intensity: f32
     points.append(&mut semi_circle_points);
     points.push(meeting_point);
 
-    let circle_start_2 = pt2(width.clone() as f32 / -4.0 + x_offset, 0.0 + y_offset);
-    let circle_end_2 = pt2(width.clone() as f32 / 4.0 + x_offset, 0.0 + y_offset);
-    let mut semi_circle_points_2 = geometry_functions::semi_circle(circle_end_2, circle_start_2);
-
-    let mut wiggler_tail_2 = get_wiggler_tail(width.clone() / 2, wiggle_rate.clone(), wiggle_intensity.clone(), tail_length.clone(), time, x_offset, y_offset);
-    let mut points_2 = Vec::with_capacity((&width + 180 + 1).try_into().unwrap());
-    let meeting_point_2 = pt2(wiggler_tail_2[0].x, wiggler_tail_2[0].y);
-    points_2.append(&mut wiggler_tail_2);
-    points_2.append(&mut semi_circle_points_2);
-    points_2.push(meeting_point_2);
-    let outer_color = hsl (x_offset.clone() / 360.0, 3.0, 20.0);
-    let inner_color = hsl (y_offset.clone() / 360.0, 3.0, 20.0);
-
-    draw.polygon()
-        .points(points)
-        .color(outer_color);
-    draw.polygon()
-        .points(points_2)
-        .color(inner_color);
+    points
 }
 
 fn view(_app: &App, _model: &Model, frame: Frame){
@@ -110,15 +102,31 @@ fn view(_app: &App, _model: &Model, frame: Frame){
     let wiggler_height = total_height as i32 / wiggler_rows - y_padding;
     let tail_length = wiggler_height as f32 * 3.5 / 5.0;
     let draw = _app.draw();
+    let x_offset: f32 = 0  as f32;
+    let y_offset: f32 = 0 as f32;
+    let outer_color = hsl (x_offset.clone() / 360.0, 1.0, 20.0);
+    let inner_color = hsl (y_offset.clone() / 360.0, 2.0, 20.0);
+    let outerPoints = get_wiggler_points(&draw, wiggler_width, 1.5, 0.9, tail_length as usize, &_app.time, &x_offset, &y_offset);
+    let innerPoints = get_wiggler_points(&draw, wiggler_width.clone() / 2, 1.5, 0.9, tail_length as usize, &_app.time, &x_offset, &y_offset);
 
-    for i in 0..(&wiggler_rows * &wiggler_columns) {
-        let row_number = (i / &wiggler_columns) as i32;
-        let column_number = i % &wiggler_columns;
-        let x_offset: f32 = min_x.clone() + (wiggler_width.clone() + x_padding.clone()) as f32 / 2.0 + (column_number * wiggler_width.clone() + x_padding.clone() * column_number)  as f32;
-        let y_offset: f32 = min_y.clone() + (wiggler_height.clone() + y_padding.clone()) as f32 / 2.0 + (row_number * wiggler_height.clone()  + y_padding.clone() * row_number.clone()) as f32;
-        //println!("row: {}, column: {}, x_offset: {}, y_offset: {}", row_number, column_number, x_offset, y_offset);
-        draw_wiggler(&draw, wiggler_width, 1.5, 0.9, tail_length as usize, &_app.time, &x_offset, &y_offset);
-    }
+
+    draw.polygon()
+        .points(outerPoints)
+        .color(outer_color);
+    draw.polygon()
+        .points(innerPoints)
+        .color(inner_color);
+    //
+    // for i in 0..(&wiggler_rows * &wiggler_columns) {
+    //     let row_number = (i / &wiggler_columns) as i32;
+    //     let column_number = i % &wiggler_columns;
+    //     let x_offset: f32 = min_x.clone() + (wiggler_width.clone() + x_padding.clone()) as f32 / 2.0 + (column_number * wiggler_width.clone() + x_padding.clone() * column_number)  as f32;
+    //     let y_offset: f32 = min_y.clone() + (wiggler_height.clone() + y_padding.clone()) as f32 / 2.0 + (row_number * wiggler_height.clone()  + y_padding.clone() * row_number.clone()) as f32;
+    //     //println!("row: {}, column: {}, x_offset: {}, y_offset: {}", row_number, column_number, x_offset, y_offset);
+    //     let outer_color = hsl (x_offset.clone() / 360.0, 3.0, 20.0);
+    //     let inner_color = hsl (y_offset.clone() / 360.0, 3.0, 20.0);
+    //
+    // }
 
     draw.to_frame(_app, &frame).unwrap();
 }
